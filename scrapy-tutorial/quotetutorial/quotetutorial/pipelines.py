8f3482c4f37603a2f9fe0f71a7f7becf8a2ed488
ParthKalkar <parthkalkar111@gmail.com>
2022-01-09 12:53:33 +0300

    update

--- scrapy-tutorial/quotetutorial/quotetutorial/pipelines.py
+++ scrapy-tutorial/quotetutorial/quotetutorial/pipelines.py
@@ -8,16 +8,19 @@
 from itemadapter import ItemAdapter
 import sqlite3
 
+
 class QuotetutorialPipeline:
 
     def __init__(self):
         self.create_connection()
         self.create_table()
 
+
     def create_connection(self):
         self.conn = sqlite3.connect("myquotes.db")
         self.curr = self.conn.cursor()
     
+
     def create_table(self):
         self.curr.execute("""DROP TABLE IF EXISTS quotes_tb""")
         self.curr.execute("""create table quotes_tb(
@@ -25,7 +28,7 @@ class QuotetutorialPipeline:
             author text,
             tag text
         )""")
-    
+
     def store_db(self,item):
         self.curr.execute("""insert into quotes_tb values (?,?,?)""",(
             item['title'][0],
